06d1058fd374a5f363b1ed1605a07133e1b24665
acapil <43251921+acapil@users.noreply.github.com>
2019-09-23 14:46:08 -0400

    angular

--- ngo-project/src/app/app.module.ts
+++ ngo-project/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-component/app.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
