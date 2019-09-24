61a4503e9f6faec8ae619b227083c46a3c77a88c
KarloF <karloantofernandez@gmail.com>
2019-09-24 13:17:40 -0400

    change

--- Django/NGO_Event_Management/event_registration/serializers.py
+++ Django/NGO_Event_Management/event_registration/serializers.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from Event.models import Event
+from .models import EventRegistration
 
 
 class EventRegistrationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Event
+        model = EventRegistration
         fields = ['id', 'user_id', 'event_id', 'first_name', 'last_name',
                   'email', 'phone', 'address', 'quantity_adult', 'quantity_kid']
