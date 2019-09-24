9aef1d7d116956d87db0120c9d24d084a3b37397
KarloF <karloantofernandez@gmail.com>
2019-09-24 04:53:22 -0400

    added get method for users

---
+++ ngo-project/src/app/user-serve.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Users } from './users';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServeService {
+  private _url: string = "http://127.0.0.1:8000/user/";
+  constructor(private http: HttpClient) { }
+
+getUsers(): Observable<Users[]>{
+
+  return this.http.get<Users[]>(this._url);
+}
+errorHandler(error: HttpErrorResponse){
+  return throwError(error.message || "Server Error");
+  }
+}
