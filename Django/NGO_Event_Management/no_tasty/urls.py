7f055a338ed4930e15adc6558db73ab5c428834a
tinhipvt <55006402+tinhipvt@users.noreply.github.com>
2019-09-25 17:46:49 -0400

    image url works
    this will show u the image http://127.0.0.1:8000/media/download_HH334aQ.png

--- Django/NGO_Event_Management/no_tasty/urls.py
+++ Django/NGO_Event_Management/no_tasty/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event_registration/', include('event_registration.urls')),
     path('user/', include('user.urls')),
     path('event/', include('event.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
