6ea09e47ce640eb003fc7a6c16ba7b8a4aa891d9
KarloF <karloantofernandez@gmail.com>
2019-09-30 14:46:21 -0400

    presentation

--- ngo-project/src/app/app-component/app.component.ts
+++ ngo-project/src/app/app-component/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ngo-project';
   key;
+
+  // navlogout() {
+  //   var header = new HttpHeaders().set('Authorization', 'Token ' + this._globals.key)
+  //   console.log('nink',this._globals.key)
+  //   this.http.post<any>('http://127.0.0.1:8000/user/auth/logout/', null, {headers: header})
+  //   this.router.navigate(['/login'])
+  // }
 }
